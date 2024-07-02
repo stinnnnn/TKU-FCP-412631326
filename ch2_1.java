56c69c38aa62fd114806f6c537b0844915710049
stinnnnn <ilovesport0704@gmail.com>
2024-07-02 12:00:03 +0800

    我的第一支 java

---
+++ ch2_1.java
@@ -0,0 +1,9 @@
+public class ch2_1 { //定義類別 ch2_1
+    public static void main(String[] args){
+        int num1=2;
+        int num2=3;
+        System.out.println(num1+"cat are running");
+        System.out.println(num2+"dogs are runnung");
+    }
+    
+}
