7e6d642f065215412f5d178fecfaefad4637a816
stinnnnn <ilovesport0704@gmail.com>
2024-07-08 14:38:45 +0800

    Add files via upload

---
+++ ch3_9_1.java
@@ -0,0 +1,8 @@
+public class ch3_9_1 {
+    public static void main(String[] args) {
+        char c1='\u4F60';
+        char c2='\u597D';
+        System.out.print(c1);
+        System.out.print(c2);
+    }
+}
